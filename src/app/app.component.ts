3f46c68bcf293575d6c16dd66afeb5e09ad4e835
Santiago <santiagodmaldon@gmail.com>
2022-10-07 10:43:54 -0400

    Search Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
-
 interface Coin{
   id : string;
   name: string;
@@ -17,7 +16,9 @@ interface Coin{
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  coinToSearch : string = '';
   coins : Coin[] = [];
+  filteredCoins : Coin[] = [];
   titles : string[] = [
     '#',
     '',
@@ -32,13 +33,21 @@ export class AppComponent implements OnInit {
     this.http.get<Coin[]>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .subscribe(
       (res) => {
-        console.log(res)
         this.coins =  res;
+        this.filteredCoins = res;
       },
       (err) => {
         console.log(err)
       }
     )
+  }
+
+  searchCoin(){
+    this.filteredCoins = this.coins.filter(coin => 
+      coin.name.toLocaleLowerCase().includes(this.coinToSearch.toLowerCase()) ||
+      coin.symbol.toLocaleLowerCase().includes(this.coinToSearch.toLowerCase()));
+    
+
   }
    
 }
